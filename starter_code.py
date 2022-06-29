777fad2c60a5b78ac433c947b2dec58586db964b
BlackMagician109 <96339439+BlackMagician109@users.noreply.github.com>
2022-06-28 23:03:01 -0500

    Finished before first submission

--- starter_code.py
+++ starter_code.py
@@ -1,16 +1,26 @@
+from itertools import combinations
 def find_max_num_balanced_substrings(l, r):
   max_num_substrings = 0
   # TODO: Add logic to find the maximum possible number of balanced non-empty
   # substrings
+  
+  num = min(l,r)
+  ls = []
+  for i in range(num):
+    ls.append(i)
+  for i in range(num):
+    combi = combinations(ls,i)
+    max_num_substrings = max_num_substrings + len(list(combi))
+
   return max_num_substrings
 
 
 def main():
   # Get number of test cases
-  test_cases = int(raw_input())
+  test_cases = int(input())
   for test_case in range(1, test_cases + 1):
     # Get total of left and right parentheses
-    l, r = map(int, raw_input().split())
+    l, r = map(int, input().split())
     ans = find_max_num_balanced_substrings(l, r)
     print("Case #%d: %d" % (test_case, ans))
 
